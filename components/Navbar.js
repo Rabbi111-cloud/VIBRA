1d9f7d698ee4a167995e6c2700db9c8d7c05f486
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-10 20:57:58 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="border-b">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between">
-        <h1 className="font-bold text-xl text-indigo-600">ToneGuard</h1>
+        <h1 className="font-bold text-xl text-indigo-600">VIBRA</h1>
         <div className="space-x-4">
           <a href="/login" className="text-gray-600 hover:text-black">Login</a>
           <a href="/signup" className="px-4 py-2 bg-indigo-600 text-white rounded">
